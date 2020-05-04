dc9d201e19ca381fd3d03828411f7e259db177dd
tap0212 <tapan.awasthi02@gmail.com>
2020-05-04 19:59:30 +0530

    save

--- controllers/event.js
+++ controllers/event.js
@@ -4,19 +4,7 @@ const _ = require("lodash")
 const fs = require("fs");
 
 
-exports.getProductById = (req,res,next) => {
-    Product.findById(id)
-    .populate("category")
-    .exec((err,product) => {
-        if(err){
-            res.status(400).json({
-                error:"Product not found"
-            })
-        }
-        req.product = product;
-        next();
-    })
-} 
+
 
 exports.createProduct = (req,res) => {
     let form = new formidable.IncomingForm();
@@ -69,77 +57,18 @@ exports.createProduct = (req,res) => {
 }
 
 
-exports.getProduct = (req,res) => {
-    req.product.photo = undefined;
-    return res.json(req.product)
-}
+
 
 //middleware
-exports.photo = (req,res,next) => {
-    if(req.product.photo.data){
-        res.set("Content-Type", req.product.photo.contentType)
-        return res.send(req.product.photo.data)
-    }
-    next()
-}
+// exports.photo = (req,res,next) => {
+//     if(req.product.photo.data){
+//         res.set("Content-Type", req.product.photo.contentType)
+//         return res.send(req.product.photo.data)
+//     }
+//     next()
+// }
 
 
-exports.updateProduct = (req,res) => {
-    let form = new formidable.IncomingForm();
-    form.keepExtensions = true;
-
-    form.parse(req, (err, fields, file) =>{
-        if(err){
-            return res.status(400).json({
-                error: "Problem with image"
-            });
-        }
-
-        //destructure the fields
-        //TODO: express validation
-    
-     
-        //updates
-        let product = req.product;
-        product = _.extend(product, fields)
-        //*** */
-
-        //handle the file
-        if(file.photo){
-            if(file.photo.size > 3000000){
-                return res.status(400).json({
-                    error: "File size too big should be under 2.5MB"
-                })
-            }
-            product.photo.data = fs.readFileSync(file.photo.path)
-            product.photo.contentType = file.photo.type;
-        }
-        product.save((err,product) => {
-            if(err){
-                return res.status(400).json({
-                    error: "Can't Update"
-                })
-            }
-            res.json(product)
-        })
-    })
-}
-
-exports.removeProduct = (req,res) => {
-    const product = req.product;
-
-  product.remove((err, deletedProduct) => {
-    if (err) {
-      return res.status(400).json({
-        error: "Failed to delete this product"
-      });
-    }
-    res.json({
-      message: "Successfully deleted",
-      deletedProduct
-    });
-  });
-}
 
 
 exports.getAllProducts = (req,res) => {
@@ -160,34 +89,4 @@ exports.getAllProducts = (req,res) => {
         })
 }
 
-exports.getAllUniqueCategories = (req,res) => {
-    Product.distinct("category",{},(err,category) => {
-        if(err){
-            return res.status(400).json({
-                error: "No category found"
-            })
-        }
-        res.json(category)
-    })
-}
 
-//update stock middleware
-exports.updateStock = (req,res,next) => {
-        let myOperations = req.body.order.products.map(prod => {
-            return {
-                updateOne: {
-                    filter: {_id: prod._id}, //find
-                    update: {$inc: {stock: -prod.count,sold: +prod.count}} //update
-                }
-            }
-        })
-        Product.bulkWrite(myOperations,{},(err, products) => {
-            if(err){
-                return res.status(400).json({
-                    error: "Bulk operation failed"
-                })
-            }
-
-            next();
-        })
-}
